b3de7292c5bd1e42a3bd90a4612f9d9c0649e7d4
alicalimli <alicalimli76@gmail.com>
2022-11-18 12:31:21 +0300

    [REM] useEffect in App tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
 import React, { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = "https://buttons.github.io/buttons.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return <div></div>;
 }
 
