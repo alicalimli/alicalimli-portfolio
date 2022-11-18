b49adb21741e05df9610db2ab9403c5ea5a22a25
alicalimli <alicalimli76@gmail.com>
2022-11-18 14:39:21 +0300

    [ADD] Projects Section (WIP)

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -2,13 +2,38 @@ import React from "react";
 
 const navLinksArr = ["Projects", "About", "Contact"];
 
+const featuredProjectsArr = [
+  {
+    name: "Moviematrix",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "ACMessenger",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
 const Hero = () => {
   const navLinks = navLinksArr.map((navLink) => (
     <li className="text-text-main text-lg">{navLink}</li>
   ));
 
+  const featuredProjects = featuredProjectsArr.map((proj) => (
+    <li key={proj.name} className="flex flex-col w-full gap-4 ">
+      <div className="w-full h-96 inline-block bg-bg-secondary"></div>
+      <div className="flex flex-col">
+        <span className="text-sm  text-muted-secondary">
+          Design & Development
+        </span>
+        <h2 className="text-text-main text-4xl font-bold">{proj.name}</h2>
+
+        <p className="text-muted-main text-lg">{proj.desc}</p>
+      </div>
+    </li>
+  ));
+
   return (
-    <section className="py-8 px-16">
+    <section className="flex flex-col gap-8 p-8 lg:py-14 lg:px-24">
       <nav className="flex gap-4">
         <div className="flex flex-col gap-2">
           <h1 className="text-text-main font-bold text-6xl">Ali Calimli</h1>
@@ -18,6 +43,9 @@ const Hero = () => {
         </div>
         <ul className="flex gap-4 ml-auto">{navLinks}</ul>
       </nav>
+      <ul className="grid gap-8 grid-cols-1 md:grid-cols-2">
+        {featuredProjects}
+      </ul>
     </section>
   );
 };
