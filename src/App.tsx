1a5a3e603f75d49ca2b6575560e3c8ea76eb041a
alicalimli <alicalimli76@gmail.com>
2022-11-18 12:28:53 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = "https://buttons.github.io/buttons.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return <div></div>;
+}
+
+export default App;
