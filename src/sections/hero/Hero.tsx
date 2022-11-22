fbd47e4ca3d1e94e6cbc8e358fdb0562bbfac2aa
alicalimli <alicalimli76@gmail.com>
2022-11-22 14:51:40 +0300

    [ADD] navbar component

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,8 +1,6 @@
 import { acmessenger, moviematrix } from "../../assets";
-import { Spantext } from "../../components";
+import Navbar from "./Navbar";
 import ProjectCard from "./ProjectCard";
-
-const navLinksArr = ["Projects", "About", "Contact"];
 
 const featuredProjectsArr = [
   {
@@ -19,32 +17,14 @@ const featuredProjectsArr = [
   },
 ];
 
-const nameArr = "Ali Calimli".split("");
-
 const Hero = () => {
-  const navLinks = navLinksArr.map((navLink, i) => (
-    <li key={navLink + i} className="text-lg text-text-main">
-      {navLink}
-    </li>
-  ));
-
-  const name = nameArr.map((letter: string) => <Spantext letter={letter} />);
-
   const featuredProjects = featuredProjectsArr.map((proj) => (
     <ProjectCard proj={proj} />
   ));
 
   return (
     <section className="flex flex-col gap-8 p-8 lg:py-14 lg:px-24">
-      <nav className="flex gap-4">
-        <div className="flex flex-col gap-2">
-          <div>{name}</div>
-          <h1 className="max-w-xs  text-xl text-muted-main">
-            Web Developer that focuses on front-end development
-          </h1>
-        </div>
-        <ul className="ml-auto flex gap-4">{navLinks}</ul>
-      </nav>
+      <Navbar />
       <ul className="fitcol grid grid-cols-1 gap-16 md:grid-cols-2">
         {featuredProjects}
       </ul>
