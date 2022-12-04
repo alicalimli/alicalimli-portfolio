b50f8103c3e051d72ac9a4dc6f231246a4a684df
alicalimli <alicalimli76@gmail.com>
2022-12-04 20:42:43 +0300

    [ADD] reusable section class

--- src/sections/contact/Contact.tsx
+++ src/sections/contact/Contact.tsx
@@ -24,39 +24,41 @@ const Contact = () => {
   ));
 
   return (
-    <section className="flex gap-16 p-8 lg:py-14 lg:px-24">
-      <div className="flex w-['50vw'] flex-col gap-12">
-        <header className="flex gap-4">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-6xl font-bold text-text-main">
-              Lets Collaborate!
-            </h1>
-            <h1 className="max-w-xl  text-xl text-muted-main">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Cupiditate, consectetur!
-            </h1>
+    <section className="section ">
+      <main className="flex gap-16">
+        <div className="flex w-['50vw'] flex-col gap-12">
+          <header className="flex gap-4">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-6xl font-bold text-text-main">
+                Lets Collaborate!
+              </h1>
+              <h1 className="max-w-xl  text-xl text-muted-main">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate, consectetur!
+              </h1>
+            </div>
+          </header>
+          <ContactForm />
+        </div>
+        <div className="flex flex-col gap-4">
+          <img src={profile} className="h-28 w-28 rounded-full" />
+          <div>
+            <h3 className="text-3xl text-text-main">Email</h3>
+            <a
+              href="mailto:alicalimli76@gmail.com"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="text-muted-main"
+            >
+              alicalimli76@gmail.com
+            </a>
           </div>
-        </header>
-        <ContactForm />
-      </div>
-      <div className="flex flex-col gap-4">
-        <img src={profile} className="h-28 w-28 rounded-full" />
-        <div>
-          <h3 className="text-3xl text-text-main">Email</h3>
-          <a
-            href="mailto:alicalimli76@gmail.com"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="text-muted-main"
-          >
-            alicalimli76@gmail.com
-          </a>
+          <div>
+            <h3 className="text-3xl text-text-main">Socials</h3>
+            <ul className="flex flex-col">{socials}</ul>
+          </div>
         </div>
-        <div>
-          <h3 className="text-3xl text-text-main">Socials</h3>
-          <ul className="flex flex-col">{socials}</ul>
-        </div>
-      </div>
+      </main>
     </section>
   );
 };
