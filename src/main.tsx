8734e6994c50c3c721f45a22caeadedc000c63e6
alicalimli <alicalimli76@gmail.com>
2022-12-05 15:25:30 +0300

    [FEAT] seperate tailwindcss file and css style files

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./main.css";
 import { ContextProvider } from "./setup";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AboutPage, ContactPage, ProjectsPage } from "./pages";
