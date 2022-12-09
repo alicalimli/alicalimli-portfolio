3c5cce6f28336e4018b27cbb670af950853781df
alicalimli <alicalimli76@gmail.com>
2022-12-09 18:18:14 +0300

    [REF] Springy text folder to springy-text

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import CustomCursor from "./custom-cursor/CustomCursor";
-import SpringyText from "./SpringyText/SpringyText";
+import SpringyText from "./springy-text/SpringyText";
 import Chip from "./chip/Chip";
 import PrimaryButton from "./primary-button/PrimaryButton";
 import Navbar from "./navbar/Navbar";
