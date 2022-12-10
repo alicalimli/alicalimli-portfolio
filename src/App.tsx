0bb8451cd3e9364dd9700af09a5c8e33aa4b2e45
alicalimli <alicalimli76@gmail.com>
2022-12-10 23:16:58 +0300

    [FIX] other pages not scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { motion } from "framer-motion";
-import React, { useEffect, useRef, useState } from "react";
-import { CustomCursor, HamburgerMenu } from "./components";
+import { CustomCursor } from "./components";
 import { About, Contact, Footer, Projects } from "./sections";
 
 function App() {
-  const [showNavBtn, setShowNavBtn] = useState(false);
-
-  const scrollContainerRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    const container = scrollContainerRef.current;
-
-    if (!container) return;
-
-    const tailwindSMBreakpoint = 640;
-    const isMobile = window.innerWidth < tailwindSMBreakpoint;
-
-    isMobile
-      ? setShowNavBtn(true)
-      : container.addEventListener("scroll", (e) => {
-          const scrolled = container.scrollTop;
-          setShowNavBtn(scrolled > 400 ? true : false);
-        });
-  }, []);
-
   return (
-    <main ref={scrollContainerRef} className="h-screen overflow-y-scroll">
-      <HamburgerMenu showNavBtn={showNavBtn} setShowNavBtn={setShowNavBtn} />
+    <>
       <CustomCursor />
       <Projects />
       <About />
       <Contact />
       <Footer />
-    </main>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import "./main.css";
 import { ContextProvider } from "./setup";
+import { useEffect, useRef, useState } from "react";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import {
   AboutPage,
@@ -13,24 +14,56 @@ import {
   Success,
 } from "./pages";
 import Layout from "./setup/router/Layout";
+import { HamburgerMenu } from "./components";
+
+const Main = () => {
+  const [showNavBtn, setShowNavBtn] = useState(false);
+
+  const scrollContainerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+
+    if (!container) return;
+
+    const tailwindSMBreakpoint = 640;
+    const isMobile = window.innerWidth < tailwindSMBreakpoint;
+
+    isMobile
+      ? setShowNavBtn(true)
+      : container.addEventListener("scroll", (e) => {
+          const scrolled = container.scrollTop;
+          setShowNavBtn(scrolled > 400 ? true : false);
+        });
+  }, []);
+  return (
+    <React.StrictMode>
+      <ContextProvider>
+        <BrowserRouter>
+          <main ref={scrollContainerRef} className="h-screen overflow-y-scroll">
+            <HamburgerMenu
+              showNavBtn={showNavBtn}
+              setShowNavBtn={setShowNavBtn}
+            />
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="success" element={<Success />} />
+              <Route element={<Layout />}>
+                <Route path="projects">
+                  <Route index element={<ProjectsPage />} />
+                  <Route path=":projectName" element={<ProjectPage />} />
+                </Route>
+                <Route path="contact" element={<ContactPage />} />
+                <Route path="about" element={<AboutPage />} />
+              </Route>
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </ContextProvider>
+    </React.StrictMode>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="success" element={<Success />} />
-          <Route element={<Layout />}>
-            <Route path="projects">
-              <Route index element={<ProjectsPage />} />
-              <Route path=":projectName" element={<ProjectPage />} />
-            </Route>
-            <Route path="contact" element={<ContactPage />} />
-            <Route path="about" element={<AboutPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ContextProvider>
-  </React.StrictMode>
+  <Main />
 );
