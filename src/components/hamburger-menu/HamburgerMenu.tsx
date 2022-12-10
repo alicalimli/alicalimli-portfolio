b1c9d78edc858215972ffd0c54fe5b603f7f892c
alicalimli <alicalimli76@gmail.com>
2022-12-10 21:57:55 +0300

    [FEAT] show navbar on scroll in bigger screens

--- src/components/hamburger-menu/HamburgerMenu.tsx
+++ src/components/hamburger-menu/HamburgerMenu.tsx
@@ -1,75 +1,102 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import NavigationMenu from "./NavigationMenu";
+
+const navBtnVariant = {
+  initial: { scale: 0 },
+  animate: { scale: 1 },
+};
 
 const HamburgerMenu = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [showNavBtn, setShowNavBtn] = useState(false);
 
   const navBtnHandler = () => setShowMenu((state) => !state);
+
+  useEffect(() => {
+    const tailwindSMBreakpoint = 640;
+    const isMobile = window.innerWidth < tailwindSMBreakpoint;
+
+    isMobile
+      ? setShowNavBtn(true)
+      : window.addEventListener("scroll", (e) => {
+          const scrolled = window.scrollY;
+          setShowNavBtn(scrolled > 400 ? true : false);
+        });
+  }, []);
+
   return (
     <>
-      <button
-        onClick={navBtnHandler}
-        className="
-        fixed
-        right-0
-        z-30
-        my-8
-        mx-4
-        flex
-        h-20
-        w-20
-        flex-col
-        items-center
-        justify-center
-        gap-2
-        overflow-hidden
-        rounded-full
-        bg-[#111]
-        sm:hidden
-      "
-      >
-        <motion.div
-          animate={
-            showMenu ? { rotate: "45deg", y: "10px" } : { rotate: "0deg" }
-          }
-          className="
-          h-0.5
-          w-8
-          bg-muted-secondary
-        "
-          transition={{
-            duration: 0.45,
-            ease: "easeInOut",
-          }}
-        />
-        <motion.div
-          animate={showMenu ? { width: 0 } : { width: "2rem" }}
-          className="
-          h-0.5
-          w-8
-          bg-muted-secondary
-        "
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-          }}
-        />
-        <motion.div
-          animate={
-            showMenu ? { rotate: "-45deg", y: "-10px" } : { rotate: "0deg" }
-          }
-          className="
-          h-0.5
-          w-8
-          bg-muted-secondary
-        "
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-          }}
-        />
-      </button>
+      <AnimatePresence>
+        {showNavBtn && (
+          <motion.button
+            key="nav-btn"
+            onClick={navBtnHandler}
+            variants={navBtnVariant}
+            initial="initial"
+            animate="animate"
+            exit="initial"
+            className="
+              fixed
+              right-0
+              z-30
+              my-8
+              mx-4
+              flex
+              h-20
+              w-20
+              flex-col
+              items-center
+              justify-center
+              gap-2
+              overflow-hidden
+              rounded-full
+              bg-[#111]
+            "
+          >
+            <motion.div
+              animate={
+                showMenu ? { rotate: "45deg", y: "10px" } : { rotate: "0deg" }
+              }
+              className="
+                h-0.5
+                w-8
+                bg-muted-secondary
+              "
+              transition={{
+                duration: 0.45,
+                ease: "easeInOut",
+              }}
+            />
+            <motion.div
+              animate={showMenu ? { width: 0 } : { width: "2rem" }}
+              initial={{ width: "2rem" }}
+              className="
+                h-0.5
+                bg-muted-secondary
+              "
+              transition={{
+                duration: 0.3,
+                ease: "easeInOut",
+              }}
+            />
+            <motion.div
+              animate={
+                showMenu ? { rotate: "-45deg", y: "-10px" } : { rotate: "0deg" }
+              }
+              className="
+                h-0.5
+                w-8
+                bg-muted-secondary
+              "
+              transition={{
+                duration: 0.3,
+                ease: "easeInOut",
+              }}
+            />
+          </motion.button>
+        )}
+      </AnimatePresence>
       <NavigationMenu showMenu={showMenu} setShowMenu={setShowMenu} />
     </>
   );
