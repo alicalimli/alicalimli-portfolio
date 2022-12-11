ee80e845ccda744a30ca3dba946a4348546eb28b
alicalimli <alicalimli76@gmail.com>
2022-12-11 17:00:47 +0300

    [FEAT] Reusable TWLink

--- src/components/tw-link/TwLink.tsx
+++ src/components/tw-link/TwLink.tsx
@@ -1,40 +1,60 @@
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
 interface TwLinkProps {
-  name: string;
-  path: string;
+  children: React.ReactNode;
+  href?: string;
+  to?: string;
+  className?: string;
   active?: boolean;
 }
 
-const TwLink = ({ name, path, active = false }: TwLinkProps) => {
-  return (
-    <Link
-      to={path}
-      className="
-        group 
-        relative
-        h-fit
-        py-2
-        text-lg
-        duration-300
-        text-text-main
-        hover:-translate-y-1
-      "
-    >
-      {name}
-      <div
-        className={`
-          ${active ? "w-full" : "w-0"}
-          absolute
-          bottom-0
-          h-px
-          w-0
-          duration-300
-          bg-white
-          group-hover:w-full
-        `}
-      />
+const TwLink = ({
+  children,
+  className,
+  to,
+  href,
+  active = false,
+}: TwLinkProps) => {
+  const linkClass = twMerge(`
+    group 
+    relative
+    inline-block
+    h-fit
+    py-1
+    text-sm
+    duration-300
+    text-white
+    hover:-translate-y-1
+    ${className}
+  `);
+
+  const lineTransitionClass = `
+    ${active ? "w-full" : "w-0"}
+    absolute
+    bottom-0
+    h-px
+    w-0
+    duration-300
+    bg-white
+    group-hover:w-full
+  `;
+
+  return to ? (
+    <Link to={to} className={linkClass}>
+      {children}
+      <div className={lineTransitionClass} />
     </Link>
+  ) : (
+    <a
+      rel="noopener noreferrer"
+      target="_blank"
+      href={href}
+      className={linkClass}
+    >
+      {children}
+      <div className={lineTransitionClass} />
+    </a>
   );
 };
 
