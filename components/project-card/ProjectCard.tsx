064701c0f1bd7aac0d9c024e713119340fd576ef
alicalimli <alicalimli76@gmail.com>
2024-12-30 23:09:15 +0300

    fix

--- components/project-card/ProjectCard.tsx
+++ components/project-card/ProjectCard.tsx
@@ -24,78 +24,26 @@ const ProjectCard = ({ proj }: ProjectCardProps) => {
       onMouseLeave={projectMouseLeave}
       rel="noopener noreferrer"
       target="_blank"
-      className="group flex flex-col gap-4 "
-      href={proj.deployed_link}
+      className="group flex flex-col gap-4"
+      href={proj.link}
     >
-      <div
-        className="
-            flex 
-            w-full 
-            flex-col 
-            gap-8
-          "
-      >
-        <div
-          className="
-              relative 
-              inline-block 
-              h-72 w-full 
-              rounded-3xl
-              bg-bg-secondary 
-              sm:h-96
-            "
-        >
+      <div className="flex w-full flex-col gap-8">
+        <div className="relative inline-block h-72 w-full rounded-3xl bg-bg-secondary sm:h-96">
           <Image
             unoptimized
-            src={proj.images[0]}
-            alt={`${proj.name} screenshot`}
+            src={proj.thumbnail}
+            alt={`${proj.title} screenshot`}
             layout="fill"
             objectFit="contain"
-            className="
-                  rounded-2xl
-                  px-6 py-12 
-                  duration-400
-                  ease-in-out
-                  group-hover:scale-105
-                  lg:py-12 
-                  lg:px-8
-                "
+            className="rounded-2xl px-6 py-12 duration-400 ease-in-out group-hover:scale-105 lg:py-12 lg:px-8"
           />
-          <div
-            className="
-                pointer-events-none 
-                absolute 
-                inset-0 
-                z-10
-                h-full
-                w-full 
-                opacity-20
-                duration-400 
-                bg-bg-main
-                group-hover:opacity-0
-              "
-          />
+          <div className="pointer-events-none absolute inset-0 z-10 h-full w-full opacity-20 duration-400 bg-bg-main group-hover:opacity-0" />
         </div>
         <div className="flex flex-col">
           <div className="flex items-start">
             <div className="flex flex-col">
-              {" "}
-              <span
-                className="
-                text-sm  
-                text-muted-secondary
-              "
-              >
-                {proj.role}
-              </span>
-              <h2
-                className="
-                  break-all 
-                  text-4xl 
-                  font-bold 
-                  text-text-main
-                  "
-              >
+              <span className="text-sm text-muted-secondary">{proj.role}</span>
+              <h2 className="break-all text-4xl font-bold text-text-main">
                 {proj.name}
               </h2>
             </div>
@@ -114,7 +62,7 @@ const ProjectCard = ({ proj }: ProjectCardProps) => {
           <p className="mt-4 text-lg text-muted-main">{proj.description}</p>
         </div>
       </div>
-      <Chip texts={proj.tools} />
+      <Chip texts={proj.technologies} />
     </a>
   );
 };
--- config/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL as string;
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
-
-export const supabase = createClient(BACKEND_URL, API_KEY);
-
-export default supabase;
