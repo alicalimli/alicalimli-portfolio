f03a0df64afd6768517038702f2be0c3d64de0b6
alicalimli <alicalimli76@gmail.com>
2025-01-14 14:11:56 +0300

    more cleanup

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,3 @@
-import acmessenger from "./images/acmessenger.png";
-import moviematrix from "./images/moviematrix.png";
-import profile from "./images/profile.jpeg";
 import codingBoy from "./lottie/95348-coding-boy.json";
 
-export { acmessenger, moviematrix, profile, codingBoy };
+export { codingBoy };
